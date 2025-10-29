a7902114a6a5b85732e9ee6bc7a80dd82e9c461e
menahem_erlich <erlichmenahem@gmail.com>
2025-10-29 14:49:00 +0200

    i finished funcsion 'column_is_full', it's work.

--- c4/board.py
+++ c4/board.py
@@ -1,12 +1,17 @@
 
-def create_board(cols: int = 7, rows: int = 6) -> list[list[str]]:
+def create_board(rows: int = 7, cols: int = 6) -> list[list[str]]:
     board = []
-    for i in range(cols):
+    for i in range(rows):
         board.append([])
-        for j in range(rows):
+        for j in range(cols):
             board[i].append("_")
     return board
 
+def column_is_full(board: list[list[str]], col: int) -> bool:
+    for i in range(len(board)):
+        if board[i][col] == "_":
+            return False
+    return True
 
 
 
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from c4.board import create_board
+from c4.board import create_board, column_is_full
 
 board = create_board()
+for i in range(len(board)):
+    board[i][2] = 'v'
 for i in board:
     for j in i:
         print(j, end=" ")
     print()
-
+print(column_is_full(board, 3))
