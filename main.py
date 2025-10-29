dca9e9c009d66f92b2b466cc3748f465c806490b
menahem_erlich <erlichmenahem@gmail.com>
2025-10-29 17:10:08 +0200

    i finished funcsion 'בדיקה אלכסונית', it's work.

--- main.py
+++ main.py
@@ -1,23 +1,27 @@
 from c4.board import create_board, column_is_full, drop_disc, render
-from c4.rules import in_bounds, balanced_test, vertical_test
+from c4.rules import in_bounds, balanced_test, vertical_test, diagonal_test_1, diagonal_test_2
 
 board = create_board()
-
-print(drop_disc(board, 1, "o"))
-print(drop_disc(board, 2, "x"))
-print(drop_disc(board, 2, "o"))
-print(drop_disc(board, 3, "x"))
-print(drop_disc(board, 3, "x"))
-print(drop_disc(board, 3, "o"))
-print(drop_disc(board, 4, "x"))
-print(drop_disc(board, 4, "x"))
-print(drop_disc(board, 4, "x"))
-print(drop_disc(board, 4, "o"))
+(drop_disc(board, 2, "x"))
+(drop_disc(board, 2, "o"))
+(drop_disc(board, 3, "x"))
+(drop_disc(board, 3, "x"))
+(drop_disc(board, 3, "o"))
+(drop_disc(board, 4, "x"))
+(drop_disc(board, 4, "x"))
+(drop_disc(board, 4, "x"))
+(drop_disc(board, 4, "x"))
+(drop_disc(board, 5, "x"))
+(drop_disc(board, 5, "x"))
+(drop_disc(board, 5, "x"))
+(drop_disc(board, 5, "x"))
+(drop_disc(board, 5, "o"))
 
 
 render(board)
 print(in_bounds(board, 6))
-print(balanced_test(board, 6, "o"))
-print(vertical_test(board, 2, "o"))
-
+# print(balanced_test(board, 6, ))
+# print(vertical_test(board, 2, ))
+print(diagonal_test_1(board, 5, 2))
+print(diagonal_test_2(board, 5, 2))
 
