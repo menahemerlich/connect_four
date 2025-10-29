ccdf128bc821b2a277d3f47f3a3a47ed181178b0
menahem_erlich <erlichmenahem@gmail.com>
2025-10-29 16:09:42 +0200

    i finished funcsion 'בדיקה אנכית ואופקית', it's work.

--- c4/rules.py
+++ c4/rules.py
@@ -3,3 +3,29 @@ def in_bounds(board: list[list[str]], col: int) -> bool:
     if col > len(board[0]):
         return False
     return True
+
+def balanced_test(board: list[list[str]], row: int, mark: str) -> bool:
+    test = ""
+    mark_test = mark * 4
+    for i in range(len(board[0])):
+        test += board[row][i]
+    if mark_test in test:
+        return True
+    return False
+
+def vertical_test(board: list[list[str]], col: int, mark: str) -> bool:
+    test = ""
+    mark_test = mark * 4
+    for i in range(len(board)):
+        test += board[i][col]
+    if mark_test in test:
+        return True
+    return False
+
+def diagonal_test(board: list[list[str]], row: int, col: int, mark: str)
+    pass
+
+
+
+
+
--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 from c4.board import create_board, column_is_full, drop_disc, render
-from c4.rules import in_bounds
+from c4.rules import in_bounds, balanced_test, vertical_test
 
 board = create_board()
 
-print(render(board))
-print(drop_disc(board, 2, "o"))
-
+print(drop_disc(board, 1, "o"))
 print(drop_disc(board, 2, "x"))
+print(drop_disc(board, 2, "o"))
+print(drop_disc(board, 3, "x"))
+print(drop_disc(board, 3, "x"))
+print(drop_disc(board, 3, "o"))
+print(drop_disc(board, 4, "x"))
+print(drop_disc(board, 4, "x"))
+print(drop_disc(board, 4, "x"))
+print(drop_disc(board, 4, "o"))
 
-for i in board:
-    for j in i:
-        print(j, end=" ")
-    print()
+
+render(board)
 print(in_bounds(board, 6))
+print(balanced_test(board, 6, "o"))
+print(vertical_test(board, 2, "o"))
 
 
